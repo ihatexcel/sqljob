c99bbf3a2df4e16f55a0df66080d5760ca28a847
ihatexcel <ihatexcel@users.noreply.github.com>
2026-03-26 21:39:50 +0000

    deploy: 9d29ef3d994569281d6d8e66033cd535d57d6533

--- dev/dist-cdn/sqljob.js
+++ dev/dist-cdn/sqljob.js
@@ -142781,7 +142781,7 @@ const createPagesSlice = (At, yt) => ({
   },
   shouldShowCell(xt) {
     const { devMode: wt } = yt();
-    return wt || ConfigManager.getCellQueryClientVisible(xt, 0) ? !0 : xt.type === "buttonRunNextCells" ? !!xt.buttonLabel : xt.type === "sql" ? ConfigManager.getCellQueryShowResult(xt, 0) : ["table", "iframe", "sqlStat"].includes(xt.type) ? xt._status === "success" || xt._status === "running" || xt._results && xt._results.length > 0 : !0;
+    return wt || ConfigManager.getCellQueryShowQueryEditor(xt, 0) ? !0 : xt.type === "buttonRunNextCells" ? !!xt.buttonLabel : xt.type === "sql" ? ConfigManager.getCellQueryShowResult(xt, 0) : ["table", "iframe", "sqlStat"].includes(xt.type) ? xt._status === "success" || xt._status === "running" || xt._results && xt._results.length > 0 : !0;
   },
   shouldShowGroup(xt) {
     const { devMode: wt } = yt();
